00bee0759abd60f85ed2fec9a44025003acc9bc1
dabui44 <dabd@anlab.pro>
2019-04-22 13:44:25 +0700

    first commit

---
+++ Animals/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver.Core;
+using System.Configuration;
+using Animals.App_Start;
+using Animals.Models;
+using MongoDB.Driver;
+
+namespace Animals.Controllers
+{
+    public class HomeController : Controller
+    {
+        private LoginContext LoginContext;
+
+        private IMongoCollection<LoginModels> loginCollection;
+
+        public HomeController()
+        {
+            LoginContext = new LoginContext();
+            loginCollection = LoginContext.database.GetCollection<LoginModels>("logins");
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModels login)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                loginCollection.InsertOne(login);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Animals/Models/LoginModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Web;
+
+namespace Animals.Models
+{
+    public class LoginModels
+    {  
+        [BsonId]
+        public ObjectId id { get; set; }
+        [BsonElement("Username")]
+        public string Username { get; set; }
+        [BsonElement("Password")]
+        public string Password { get; set; }
+    }
+}
